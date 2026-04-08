1ee154fe502faf7813e7f1767cae5148abbf0d94
Rafael <rafael1893porto@gmail.com>
2026-04-08 15:38:37 +0200

    add user details service

---
+++ src/main/java/com/rafael/todolist/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.rafael.todolist.service;
+
+import com.rafael.todolist.entity.User;
+import com.rafael.todolist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
