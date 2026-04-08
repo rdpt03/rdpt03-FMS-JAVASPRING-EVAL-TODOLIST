7e14be2375f50c20df06acfb0383eaae2ae810b5
Rafael <rafael1893porto@gmail.com>
2026-04-08 15:38:09 +0200

    add class to manage auth

---
+++ src/main/java/com/rafael/todolist/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.rafael.todolist.controller;
+
+import com.rafael.todolist.entity.User;
+import com.rafael.todolist.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.security.autoconfigure.SecurityProperties;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    //---------------------REGISTER--------------------------
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String username,
+                               @RequestParam String password,
+                               Model model) {
+
+        if (repo.findByUsername(username).isPresent()) {
+            model.addAttribute("error", "Usuário já existe");
+            return "register";
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+        repo.save(user);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register"; // Thymeleaf template
+    }
+
+    //-----------------------login--------------------------
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login"; // Thymeleaf template
+    }
+
+
+}
