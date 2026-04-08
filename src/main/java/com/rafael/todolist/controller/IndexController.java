29e0cead1204fdce1abd83d975b7a9ee9ffb4c3f
Rafael <rafael1893porto@gmail.com>
2026-04-08 15:44:14 +0200

    call index page

---
+++ src/main/java/com/rafael/todolist/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.rafael.todolist.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String home(Authentication authentication, Model model) {
+        return "index";
+    }
+}
