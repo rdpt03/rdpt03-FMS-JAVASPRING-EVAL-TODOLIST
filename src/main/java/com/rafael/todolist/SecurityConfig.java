edfe9b62d139344464f0cd41e1784d802e79d8be
Rafael <rafael1893porto@gmail.com>
2026-04-09 13:08:24 +0200

    add exception

--- src/main/java/com/rafael/todolist/SecurityConfig.java
+++ src/main/java/com/rafael/todolist/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/register", "/login", "/css/**").permitAll()
