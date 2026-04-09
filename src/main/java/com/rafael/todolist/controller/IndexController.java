1f7aa3346d7155f1a31e78a01234ff9e01fb86f3
Rafael <rafael1893porto@gmail.com>
2026-04-09 14:19:04 +0200

    add check to see if user is connected fix tabulations

--- src/main/java/com/rafael/todolist/controller/IndexController.java
+++ src/main/java/com/rafael/todolist/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.rafael.todolist.controller;
 import com.rafael.todolist.entity.User;
 import com.rafael.todolist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +16,17 @@ public class IndexController {
 
     @GetMapping("/index")
     public String home(Authentication auth, Model model) {
-        //get user name
-        String username = auth.getName();
-        //get user
-        User user = userRepo.findByUsername(username).orElseThrow();
+        //check id user is connected
+        if (auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken)) {
+            //get user name
+            String username = auth.getName();
+            //get user
+            User user = userRepo.findByUsername(username).orElseThrow();
 
-        //send to front
-        model.addAttribute("user", user);
-        model.addAttribute("title", "Accueil - TDL");
-        return "index";
+            //send to front
+            model.addAttribute("user", user);
+            model.addAttribute("title", "Accueil - TDL");
+            return "index";
+        }
     }
 }
