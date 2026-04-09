ab55df6d07abfca1b763bd37009aa291e817c5c3
Rafael <rafael1893porto@gmail.com>
2026-04-09 13:09:34 +0200

    add send user to front

--- src/main/java/com/rafael/todolist/controller/IndexController.java
+++ src/main/java/com/rafael/todolist/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.rafael.todolist.controller;
 
+import com.rafael.todolist.entity.User;
+import com.rafael.todolist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserRepository userRepo;
 
     @GetMapping("/index")
-    public String home(Authentication authentication, Model model) {
+    public String home(Authentication auth, Model model) {
+        //get user name
+        String username = auth.getName();
+        //get user
+        User user = userRepo.findByUsername(username).orElseThrow();
+
+        //send to front
+        model.addAttribute("user", user);
+        model.addAttribute("title", "Accueil - TDL");
         return "index";
     }
 }
